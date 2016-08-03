19f29fe6a3331fad2ba6b9788d35f9db3ce06320
Yevgeniy Ponomarev <ponomaryov.jenya@gmail.com>
2016-08-04 00:00:41 +0600

    Update 8/3/2016: Searchs max from 5 number or more

---
+++ src/com/javarush/test/level06/lesson11/bonus03/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.test.level06.lesson11.bonus03;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* Задача по алгоритмам
+Задача: Написать программу, которая вводит с клавиатуры 5 чисел и выводит их в возрастающем порядке.
+Пример ввода:
+3
+2
+15
+6
+17
+Пример вывода:
+2
+3
+6
+15
+17
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] mas = new int[5];
+        for (int b = 0; b < 5; b++) {
+            mas[b] = Integer.parseInt(reader.readLine());
+        }
+        int prom = 0;
+        int min;
+        for (int j = 0; j < 5; j++) {
+            min = mas[j];
+            for (int i = j; i < 5; i++) {
+                if (mas[i] < min) {
+                    min = mas[i];
+                    prom = i;
+                }
+
+            }
+            mas[prom] = mas[j];
+            mas[j] = min;
+            System.out.println(mas[j]);
+        }
+    }
+}
